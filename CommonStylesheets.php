b8a63f58fbb5490e59553052d03810be5790220b
BnnQ <1v25fa94@gmail.com>
2023-06-27 19:19:20 +0300

    Fixed import paths

--- CommonStylesheets.php
+++ CommonStylesheets.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="stylesheets/bootstrap.css"/>
-<link rel="stylesheet" href="stylesheets/site.css"/>
-<link rel="stylesheet" href="node_modules/slick-carousel/slick/slick.css"/>
-<link rel="stylesheet" href="node_modules/slick-carousel/slick/slick-theme.css"/>
+<link rel="stylesheet" href="/LWHW/stylesheets/bootstrap.css"/>
+<link rel="stylesheet" href="/LWHW/stylesheets/site.css"/>
+<link rel="stylesheet" href="/LWHW/node_modules/slick-carousel/slick/slick.css"/>
+<link rel="stylesheet" href="/LWHW/node_modules/slick-carousel/slick/slick-theme.css"/>
 <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
--- Outlet.php
+++ Outlet.php
@@ -5,8 +5,8 @@ require_once 'Models/Route.php';
 require_once 'DependencyContainer.php';
 require_once 'vendor/autoload.php';
 
-use Services\Router;
 use Services\UserManagerBase;
+use Utils\Router;
 
 
 class Outlet {
@@ -22,19 +22,19 @@ $component = DependencyContainer::getContainer()->get(Outlet::class);
 <html lang="en">
 <head>
     <title>1</title>
-    <?php require_once "CommonStylesheets.php"?>
+    <?php require_once "CommonStylesheets.php" ?>
 </head>
 <header class="fixed-top">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <a class="navbar-brand" href="Outlet.php?page=home" style="display: flex; align-items: center;">
-            <img src="media/logo.png" class="img-fluid" alt="Logo">
+            <img src="/LWHW/wwwroot/media/logo.png" class="img-fluid" alt="Logo">
         </a>
         <div class="container-fluid justify-content-end" style="padding: 32px">
             <div class="row">
                 <div class="col">
                     <?php
                     if ($component->userManager->isCurrentUserAuthenticated())
-                        echo "<a href='Outlet.php?page=logout'><i class='fa fa-sign-out'></i>Log out</a>";
+                        echo "<a href='/LWHW/Outlet.php?page=logout'>Log out</a>";
                     ?>
                 </div>
             </div>
@@ -43,9 +43,9 @@ $component = DependencyContainer::getContainer()->get(Outlet::class);
     </nav>
 </header>
 <body>
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="node_modules/slick-carousel/slick/slick.min.js"></script>
+<script src="/LWHW/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/LWHW/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="/LWHW/node_modules/slick-carousel/slick/slick.min.js"></script>
 <?php
 $dependencyContainer = DependencyContainer::getContainer();
 $startup = $dependencyContainer->get(Startup::class);
