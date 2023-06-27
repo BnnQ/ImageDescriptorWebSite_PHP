c6aa23fe2aac42a1efccbf8c32b692573411bf83
BnnQ <1v25fa94@gmail.com>
2023-06-27 15:33:55 +0300

    Added stylesheet imports

---
+++ CommonStylesheets.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="stylesheets/bootstrap.css"/>
+<link rel="stylesheet" href="stylesheets/site.css"/>
+<link rel="stylesheet" href="node_modules/slick-carousel/slick/slick.css"/>
+<link rel="stylesheet" href="node_modules/slick-carousel/slick/slick-theme.css"/>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
